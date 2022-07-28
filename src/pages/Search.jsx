5f9eb55c6677069d0b755e4ba2520808ee923cac
Giovanna Helena <giovannahlopes98@gmail.com>
2022-07-27 21:35:23 -0300

    final version of MoviesLib

---
+++ src/pages/Search.jsx
@@ -0,0 +1,48 @@
+import React from "react"
+
+import {useState, useEffect} from "react";
+import {useSearchParams} from "react-router-dom";
+import MovieCard from "../components/MovieCard";
+
+
+import "./MovieGrid.css";
+
+
+const searchUrl = import.meta.env.VITE_SEARCH;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+const Search = () =>{
+
+    const [searchParams] = useSearchParams()
+
+    const [movies, setMovies] = useState([]);
+    const query = searchParams.get("q");
+
+    const getSeachedMovies = async(url) =>{
+        const res = await fetch(url);
+        const data = await res.json();
+
+        setMovies(data.results);
+    }
+
+    useEffect(()=>{
+
+        const searchWithQueryUrl = `${searchUrl}?${apiKey}&query=${query}`;
+
+        getSeachedMovies(searchWithQueryUrl);
+
+    }, [query])
+
+
+    return (
+        <div className="container">
+            <h2 className="title">Resultados para : <span className="query-text">{query}</span></h2>
+            <div className="movies-container">
+                {movies.length === 0 && <p>Carregando...</p>}
+                {movies && movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+            </div>
+        </div>
+        )
+}
+
+export default Search;
